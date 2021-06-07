e84ff9c1d11ce5ec7ae9011cbc46ab315f52b4c6
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2021-06-07 11:38:29 +0900

    Dockerイメージ作成

---
+++ src/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+
+const PORT = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.render('hello.ejs');
+});
+
+app.listen(PORT);
+console.log(`Running on http://localhost:${PORT}`);
