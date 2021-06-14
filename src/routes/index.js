fa5c9cb1a11bbbb167dd7c985256661be1f26753
kanjiro-ryu <kanjiro.ryu@rakus-partners.co.jp>
2021-06-14 09:41:25 +0900

    ルーティングをroutes/indexで管理

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router(),
+    usersRouter = require('./users'),
+    tweetsRouter = require('./tweets');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+router.use('/users', usersRouter);
+router.use('/tweet', tweetsRouter);
 
 module.exports = router;
