97405d2916c86060c0db3845d2d41c041e2005cc
kanjiro-ryu <kanjiro.ryu@rakus-partners.co.jp>
2021-06-16 14:07:11 +0900

    コード整理

--- src/controllers/tweetController.js
+++ src/controllers/tweetController.js
@@ -4,47 +4,32 @@ const Tweet = require('../models/tweetSchema'),
 exports.createTweets = async (req,res) => {
     console.log('createTweet....')
     console.log(req.body);
+    //本来であれば、ログイン中のユーザー情報をreq.userから取得する
     let user;
     User.find({},(error, result) =>{
         console.log(result)
         if( result.length !== 0 ){
             user = result[0]
-            console.log(user)
         }
         if (error) console.log(error)
-
         Tweet.create({
             user:user,
             content:req.body.content
         },(error, result )=>{
             if(error) console.log(error)
-            else
-                console.log("saved : " + result);
+            else console.log("saved : " + result);
             res.redirect('/tweets/')
         })
     })
 }
 
-exports.getAllTweets = (req,res) => {
+exports.getAllTweets = async (req,res) => {
     console.log('getAllTweets......')
-    Tweet.find({}).populate('user').exec((error, result )=>{
-        console.log(result)
-        const user = {
-            name:'testname',
-            password:'tesrpass',
-            email:'testmail'
-        }
-        // console.log(result[0]._id.getTimestamp())
-        if ( result.length === 0 ){
-            console.log("!result")
-            res.render('tweets/index', {
-                tweets:result,
-                user:user
-            })
-        }
-        if(!error)
-            // User.findById(result.user)
-            console.log("!error")
+    await Tweet.find({}).populate('user').exec((error, result )=>{
+        console.log('result : ' + result)
+        //本来であれば、ログイン中のユーザー情報をreq.userから取得する
+        const user = {　name:'testname', password:'tesrpass', email:'testmail'}
+        if(error) console.log("error")
         res.render('tweets/index', {
             tweets:result,
             user:user
