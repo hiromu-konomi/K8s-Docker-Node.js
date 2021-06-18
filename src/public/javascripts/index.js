1ae788a55b8b76856028a3171243d86043e7d762
kanjiro-ryu <kanjiro.ryu@rakus-partners.co.jp>
2021-06-18 10:35:46 +0900

    コード整理

--- src/public/javascripts/index.js
+++
@@ -1,9 +0,0 @@
-var image_onclick = function (event) {
-    window.alert('Hello World !');
-};
-
-var document_onready = function (event) {
-    $('#myButton').on('click', image_onclick);
-};
-
-$(document).ready(document_onready);
