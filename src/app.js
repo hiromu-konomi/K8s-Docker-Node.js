36836e193968cdca64467f8750fafd39f87149ad
kanjiro-ryu <kanjiro.ryu@rakus-partners.co.jp>
2021-06-18 10:38:40 +0900

    ajax,jquery反映用app.useの追加

--- src/app.js
+++ src/app.js
@@ -45,7 +45,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//ajax用
+app.use('/public' , express.static(path.join(__dirname, 'public')));
+app.use( express.static(path.join(__dirname, 'public')));
 
 // routing
 app.use("/", router);
