eaa1840df6c5df262d3ed29b1fb7718aad415fb1
kanjiro-ryu <kanjiro.ryu@rakus-partners.co.jp>
2021-06-21 11:49:29 +0900

    req.userからログインユーザーを取得

--- src/controllers/favoriteController.js
+++ src/controllers/favoriteController.js
@@ -2,56 +2,49 @@ const Tweet = require('../models/tweetSchema'),
     User = require('../models/userSchema'),
     Favorite = require('../models/favoriteSchema');
 
-exports.switchFavorite = async (req,res) => {
+exports.switchFavorite = async (req, res) => {
     console.log('switchFavorite....')
-    let tweetUser;
-    let loginUser;
-
-    User.find({},async (error, result) =>{
-        if( result.length !== 0 ){
-            tweetUser = result[0]
-            loginUser = result[1]
-            // console.log('tweetUser : ' + tweetUser)
-            // console.log('loginUser : ' + loginUser)
-            //populateを利用することで参照先のidのみならずフィールド全てを取得
-            await Favorite.find({'user': loginUser , 'tweet' : req.params.id, }).populate('tweet').exec( async (error, result) =>{
-                if (error) console.log('fav find  err : ' + error)
-                // console.log('fav find result : ' + result)
-                //フロー1
-                if(result.length === 0){
-                    //tweet._idから投稿内容を取得
-                    await Tweet.findById(req.params.id, async (err, result) =>{
-                        if(err) console.log( 'tweet findById err : ' + err )
-                        // console.log('tweet findById result : ' + result)
-                        //これらのデータをもとにfavoriteドキュメントを作成
-                        await Favorite.create({
-                            user:loginUser,
-                            tweet: result,
-                            status: true
-                        }, async (error, result )=>{
-                            if(error) console.log('fav create err : ' + error)
-                             console.log("fav create saved : " + result);
-                            res.contentType('json');
-                            res.json(result[0].status)
-                        })
+    if (req.user === undefined) {
+        console.log("no has loginUser....")
+    } else {
+        //populateを利用することで参照先のidのみならずフィールド全てを取得
+        await Favorite.find({
+            'user': req.user,//ログインユーザー
+            'tweet': req.params.id,//対象投稿内容
+        }).populate('tweet').exec(async (error, result) => {
+            if (error) console.log('fav find  err : ' + error)
+            //フロー1
+            if (result.length === 0) {
+                //tweet._idから投稿内容を取得
+                await Tweet.findById(req.params.id, async (err, result) => {
+                    if (err) console.log('tweet findById err : ' + err)
+                    //これらのデータをもとにfavoriteドキュメントを作成
+                    await Favorite.create({
+                        user: req.user,//いいねを押したユーザー
+                        tweet: result,
+                        isFavorite: true,
+                        isRead: false
+                    }, async (error, result) => {
+                        if (error) console.log('fav create err : ' + error)
+                        else {
+                            console.log("fav create saved : " + result);
+                            await res.json(result)
+                        }
                     })
-                }
-                //フロー2
-                else {
-                   console.log('status preUpdate : ' + result[0].status)
-                   if (result[0].status === true) result[0].status = false
-                   else result[0].status = true
-                   console.log('status updated : ' + result[0].status)
-                   await result[0].save()
-                    res.contentType('json');
-                    res.json(result[0].status)
-                }
-
-            })
-        }
-     })
-
-    // res.redirect('/tweets/')
+                })
+            }
+            //フロー2
+            else {
+                console.log('status preUpdate : ' + result[0].isFavorite)
+                if (result[0].isFavorite === true) result[0].isFavorite = false
+                else result[0].isFavorite = true
+                console.log('status updated : ' + result[0].isFavorite)
+                await result[0].save()
+                res.contentType('json');
+                res.json(result[0].isFavorite)
+            }
+        })
+    }
 }
 
 //フロー1：いいねボタン押下時にドキュメント作成
@@ -65,4 +58,13 @@ exports.switchFavorite = async (req,res) => {
 //取得できなかったらフロー1を行う
 
 
+//通知機能
+//いいねボタン押下時に通知を送る
+//いいねのtrue or falseの切り替え後処理を行う
+//切り替え後の値がtrueの場合に通知する
+//tweet_idから投稿したユーザーを特定する
+//通知内容を確認しているか（isRead）の値がfalseの場合、通知が来てることを分かるようにする（ページないのどこかを赤にするとか）
+
+//投稿一覧画面遷移時、通知内容が存在するか確認
+//（isReadがfalseのfavoritesドキュメントが１つでも存在したら）アイコン光らせる？
 
